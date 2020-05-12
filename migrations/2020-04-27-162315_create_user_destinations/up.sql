2c0f303cf8fcbdebc108628b4a82a5a4ef4f0bdc
Jason Reilly <jdr0887@gmail.com>
2020-05-12 09:35:50 -0400

    removing

--- migrations/2020-04-27-162315_create_user_destinations/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_destinations (
-  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  user_id INTEGER NOT NULL,
-  destination_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (destination_id) REFERENCES destinations (id)
-)
