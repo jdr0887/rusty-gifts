a051d676576701051dc8aaccb5773a79a44f8a55
Jason Reilly <jdr0887@gmail.com>
2020-05-15 22:54:32 -0400

    removing

--- server/migrations/2020-04-27-162321_create_user_gift_ideas/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_gift_ideas (
-  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  user_id INTEGER NOT NULL,
-  gift_idea_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (gift_idea_id) REFERENCES gift_ideas (id)
-)
